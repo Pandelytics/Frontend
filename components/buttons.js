12d1252dd2de49180e86400235201c6f9377db4a
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-04-06 12:34:19 +0100

    Added custom buttons

--- components/buttons.js
+++ components/buttons.js
@@ -1,1 +1,9 @@
-console.log('yo')
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach(btn => {
+  // Get only buttons that has a data attribute
+  if (Object.entries(btn.dataset).length > 0) {
+    btn.style.width = btn.dataset.width;
+    btn.style.height = btn.dataset.height;
+  }
+});
