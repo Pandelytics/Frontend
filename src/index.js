3224a022f85bf8849dd10bf9c20e2ee1565891d0
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-04-25 15:52:28 +0100

    Bootstrapped project

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
+// import './css/variables.scss';
+// import './css/component.scss';
 import './css/main.scss';
 import _ from 'lodash';
+
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,46 +1,40 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 // const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
-  devtool: 'inline-source-map',
+  entry: "./src/index.js",
+  mode: "development",
+  devtool: "inline-source-map",
   devServer: {
-    contentBase: './dist',
+    contentBase: "./dist",
   },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
-      rules: [
-          {
-              test: /\.s[ac]ss$/i,
-              use: [
-                  MiniCssExtractPlugin.loader,
-                  'css-loader',
-                  'sass-loader'
-              ],
-          },
-          {
-            test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
-            use: [
-              'file-loader',
-            ],
-          }
-      ]
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+        use: ["file-loader"],
+      },
+    ],
   },
   plugins: [
     // new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      filename: 'index.html',
+      filename: "index.html",
       inject: true,
-      template: path.resolve(__dirname, 'src', 'index.html'),
+      template: path.resolve(__dirname, "src", "index.html"),
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
+      filename: "[name].[contenthash].css",
     }),
   ],
 };
