c9d2cb698538a05127a7ace72cff920484d783df
Wizzywit <wisdompraise968@gmail.com>
2020-05-02 11:10:11 +0100

    Initialization of data visualization map on dashboard

--- src/index.js
+++ src/index.js
@@ -2,4 +2,46 @@
 // import './css/component.scss';
 import './css/main.scss';
 import _ from 'lodash';
+import $ from 'jquery';
+import '../raphael-2.3.0';
+import 'jquery-mapael';
+import 'jquery-mapael/js/maps/world_countries.js';
+
+
+
+$(document).ready(function() {
+    $(".container").mapael({
+        map : {
+            name : "world_countries",
+            defaultArea: {
+                attrs: {
+                    fill: "#ff69b4",
+                    stroke: "#fff",
+                    "stroke-width": 0.3
+                }
+            },
+            defaultPlot: {
+                text: {
+                    attrs: {
+                        fill: "#b4b4b4",
+                        "font-weight": "normal"
+                    },
+                    attrsHover: {
+                        fill: "#fff",
+                        "font-weight": "bold"
+                    }
+                }
+            },
+            zoom: {
+                enabled: true
+                , step: 0.25
+                , maxLevel: 20
+            }
+        }
+        
+    });
+    
+});
+
+
 
